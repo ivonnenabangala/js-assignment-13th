920d965e0838e29a89bf19da5d83f58dd37a332b
ivonnenabangala <ivonnenabangala@gmail.com>
2025-01-14 14:17:17 +0300

    handle edge cases for repetitive.js

--- repetitive.js
+++ repetitive.js
@@ -1,24 +1,19 @@
-function hasDuplicates(str){
-    let password = str.split('')
-    let i, j;
-    for(i = 0; i < password.length; i++){
-        for( j = 1; j < password.length; j++){
-            console.log(password[j]);
-            
+function hasConsecutiveDuplicateNumbers(str) {
+    for (let i = 0; i < str.length - 1; i++) {
+        if (!isNaN(str[i]) && str[i] === str[i + 1]) {
+            return true;
         }
-        if(password[i] != password[j]){
-            console.log(password[i]);
-            console.log('j', password[j]);
-            return false
-        } else {
-            // console.log(false);
-            
-            return true
-        }
-        
-        
     }
+    return false;
 }
-let str = '111s'
-let result = hasDuplicates(str)
-console.log(result);
+
+let str1 = '112'; 
+let str2 = '121'; 
+let str3 = 'pass12word2'; 
+let str4 = 'pass122';     
+
+console.log(hasConsecutiveDuplicateNumbers(str1)); 
+console.log(hasConsecutiveDuplicateNumbers(str2)); 
+console.log(hasConsecutiveDuplicateNumbers(str3)); 
+console.log(hasConsecutiveDuplicateNumbers(str4)); 
+
